607423a93800f6241913f191ea405bec1f08cac7
Rafał Lorenz <vardius@gmail.com>
2016-07-08 10:12:06 +0200

    Add config struct

--- main.go
+++ main.go
@@ -3,35 +3,46 @@ package main
 import (
 	"flag"
 	"net/http"
-	"golang.org/x/net/context"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/opstalent/tracker/auth"
 	"github.com/opstalent/tracker/issue"
-	"github.com/opstalent/tracker/project"
 	"github.com/opstalent/tracker/logger"
+	"github.com/opstalent/tracker/project"
 	"github.com/opstalent/tracker/router"
-	"os"
+	"github.com/vardius/env"
+	"golang.org/x/net/context"
 )
 
+type enviroment struct {
+	Username     int  `env:"USERNAME",required`
+	Password     int  `env:"PASSWORD",required`
+	IsProduction bool `env:"PRODUCTION"`
+	Port         int  `env:"PORT" envDefault:"8080"`
+	Format       int  `env:"FORMAT" envDefault:"json"`
+	Log          logger.Logger
+	Router       *mux.Router
+}
+
+var (
+	Env = &enviroment{
+		Log: logger.New(),
+	}
+)
+
+//Example program run
+//PRODUCTION=true USERNAME="user" PASSWORD="password" go run tracker.go
 func main() {
 	var (
-		ctx context.Context
+		ctx    context.Context
 		cancel context.CancelFunc
-		username string
-		password string
-		host string
-		port string
-		programPort string
-		format string
+		host   string
+		port   string
 	)
 
-	username = os.Args[1]
-	password = os.Args[2]
-
-	flag.StringVar(&programPort, "port", "8080", "Set server port, default 8080")
 	flag.StringVar(&host, "h", "redmine.ops-dev.pl", "Set host, default = redmine.ops-dev.pl")
 	flag.StringVar(&port, "p", "", "Set port, default empty")
-	flag.StringVar(&format, "f", "json", "Set format, default = json")
 	flag.Parse()
 
 	ctx, cancel = context.WithCancel(context.Background())
@@ -41,10 +52,10 @@ func main() {
 	issue.AddRoutes(ctx)
 	project.AddRoutes(ctx)
 
-	router := router.New()
+	Env.Router = router.New()
 
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./resources/")))
 
-	log := logger.New()
-	log.Critical(ctx, "%s", http.ListenAndServe(":" + programPort, router))
+	Env.Log.Critical(ctx, "%s", env.Parse(&Env))
+	Env.Log.Critical(ctx, "%s", http.ListenAndServe(":"+strconv.Itoa(Env.Port), router))
 }
